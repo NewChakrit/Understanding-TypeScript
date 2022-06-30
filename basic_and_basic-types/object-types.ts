9e465991c3884126ab43bd8b851f6e0b1d9fe71e
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-06-30 20:20:55 +0700

    object-type

---
+++ basic_and_basic-types/object-types.ts
@@ -0,0 +1,44 @@
+//Object Types
+
+const person: {
+	name: string;
+	age: number;
+} = {
+	name: 'New',
+	age: 25,
+};
+
+const person2 = {
+	name: 'New',
+	age: 25,
+};
+
+console.log(person.age);
+
+// Of course object types can also be created for nested objects.
+
+// Let's say you have this JavaScript object:
+
+const product = {
+	id: 'abc1',
+	price: 12.99,
+	tags: ['great-offer', 'hot-and-new'],
+	details: {
+		title: 'Red Carpet',
+		description: 'A great carpet - almost brand-new!',
+	},
+};
+
+// This would be the type of such an object:
+
+//   {
+//     id: string;
+//     price: number;
+//     tags: string[];
+//     details: {
+//       title: string;
+//       description: string;
+//     }
+//   }
+
+// So you have an object type in an object type so to say.
